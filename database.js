c0094807a6e0ff557a54dfb9cc6ea4a152c66412
goudete <goudetenrique@gmail.com>
2021-04-03 12:37:19 -0600

    final commit

--- database.js
+++ database.js
@@ -16,7 +16,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             )`,
             (err) => {
                 if (err) {
-                    // Table already created
+                    console.log(err)
                 }else{
                    
                 }
